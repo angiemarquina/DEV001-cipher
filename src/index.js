473f9b8c4e6f5640932fcd30d057469525e8c96b
Angie Marquina <angiemarquina65@gmail.com>
2022-10-10 09:59:38 -0500

    avance cipher 1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+const cipherBtn = document.getElementById("cipher");
+cipherBtn.addEventListener("click",()=>{
+    const text = document.getElementById("text").value;
+    const offSet = document.getElementById("offSet").value;
+    document.getElementById("cipherText").innerHTML = cipher.encode(offSet,text);
+});
+
+const decipherBtn = document.getElementById("decipher");
+decipherBtn.addEventListener("click",()=>{
+    const text = document.getElementById("text").value;
+    const offSet = document.getElementById("offSet").value;
+    document.getElementById("decipherText").innerHTML = cipher.decode(offSet,text);
+});
